900c7da934092ef2d3869d40316f715ff9c2752c
Rana Ameer <ranaameer7260@gmail.com>
2021-11-02 21:18:39 +0200

    some functionality update

--- src/Home.js
+++ src/Home.js
@@ -12,60 +12,83 @@ const Home = () => {
       var m = document.getElementById("Q");
       m.setAttribute("style", "color: #ccc;");
     }
-    if (p.search(/[a-z]/i).length > 0) {
+    if (p.match(/[a-z]/)) {
       var n = document.getElementById("R");
       n.setAttribute("style", "color: #ccc;");
     }
-    if (p.search(/[0-9]/) > 0) {
+    if (p.match(/[0-9]/)) {
       var q = document.getElementById("S");
       q.setAttribute("style", "color: #ccc;");
     }
-    if (p.search(/[A-Z]/i) > 0) {
+    if (p.match(/[A-Z]/)) {
       var r = document.getElementById("T");
       r.setAttribute("style", "color: #ccc;");
     }
 
     return true;
   }
+
   function colorMeter() {
     var colorValue = document.getElementById("myInput").value;
-    if (colorValue.length === 2) {
+    if (colorValue.length === 4) {
       setWeak(true);
     }
     if (colorValue.length === 8) {
       setMedium(true);
     }
     if (colorValue.length === 12) {
+      var u = document.getElementById("U");
+      u.setAttribute("style", "color: #ccc;");
       setStrong(true);
     }
   }
 
   return (
-    <div className="create">
-      <h1>Password Strength Meter</h1>
-      <form>
-        <input
-          type="password"
-          placeholder="Enter your password"
-          className="input"
-          id="myInput"
-          onChange={(e) => {
-            colorMeter();
-            validatePassword();
-          }}
-        ></input>
-      </form>
-      {weak && <div className="first" />}
-      {medium && <div className="second" />}
-      {strong && <div className="third" />}
+    <div className="container">
+      <div className="create">
+        <h1>Password Strength Meter</h1>
+        <form>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="input"
+            id="myInput"
+            onChange={(e) => {
+              colorMeter();
+              validatePassword();
+            }}
+          ></input>
+        </form>
+        {weak && (
+          <div className="first">
+            <br />
+            <p style={{ color: "#ff4758" }}>Weak</p>
+          </div>
+        )}
+        {medium && (
+          <div className="second">
+            <br />
+            <p style={{ color: "#ffa600" }}>Medium</p>
+          </div>
+        )}
+        {strong && (
+          <div className="third">
+            <br />
+            <p style={{ color: "#27b160" }}>Strong</p>
+          </div>
+        )}
 
-      <br />
-      <ul className="ul">
-        <li id="Q">Must contain 8 characters</li>
-        <li id="R">Must have at least one letter</li>
-        <li id="S">Must have at least one digit</li>
-        <li id="T">Must have at least one capital letter</li>
-      </ul>
+        <br />
+        <br />
+        <br />
+        <ul className="ul">
+          <li id="Q"> Must contain 8 characters</li>
+          <li id="R"> Must have at least one letter</li>
+          <li id="S"> Must have at least one digit</li>
+          <li id="T"> Must have at least one capital letter</li>
+          <li id="U"> Must be at least 12 charaters long</li>
+        </ul>
+      </div>
     </div>
   );
 };
